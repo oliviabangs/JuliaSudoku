a0fa8e137f9597985f04194d5282ddf22aa752cc
Jorge Larach <jorgelarach@jorges-laptop.trinity.local>
2024-04-18 15:19:14 -0500

    Hooked up each button to it's own callback function

--- gui.jl
+++ gui.jl
@@ -5,13 +5,16 @@ function generate_child(label::String)::Button
     button = Button(Label(label))
     set_size_request!(button, Vector2f(50, 50))
     
-    
+    connect_signal_clicked!(on_clicked, button, label)
     return button
 end
 
+function on_clicked(self::Button, data)
+    println(data)
+end
 
 main() do app::Application
-    
+    random_array = rand(1:9, 9, 9)
     window = Window(app)
     
     grid = Grid()
@@ -23,7 +26,11 @@ main() do app::Application
     
     row = 1
     column = 1
-    
+
+    # for row in eachrow(random_array)
+
+        
+
     for index in 1:9
         Mousetrap.insert_at!(grid, cells[index], column, row, 1, 1)
         column += 1
@@ -31,7 +38,6 @@ main() do app::Application
             row += 1
             column = 1
         end
-
     end
     box = hbox(grid)
     set_spacing!(box, 10)
