448602124b75a4beef9b6cab5aa0980fc70365e2
Jorge Larach <jorgelarach@jorges-laptop.trinity.local>
2024-04-18 14:45:56 -0500

    generating and displaying a 3x3 grid of buttons

--- gui.jl
+++ gui.jl
@@ -1,6 +1,40 @@
 using Mousetrap
+
+function generate_child(label::String)::Button
+    
+    button = Button(Label(label))
+    set_size_request!(button, Vector2f(50, 50))
+    
+    
+    return button
+end
+
+
 main() do app::Application
+    
     window = Window(app)
-    set_child!(window, Label("Hello World!"))
+    
+    grid = Grid()
+
+    cells = []
+    for i in 1:9
+        push!(cells, generate_child(string(i)))
+    end
+    
+    row = 1
+    column = 1
+    
+    for index in 1:9
+        Mousetrap.insert_at!(grid, cells[index], column, row, 1, 1)
+        column += 1
+        if index % 3 == 0
+            row += 1
+            column = 1
+        end
+
+    end
+    box = hbox(grid)
+    set_spacing!(box, 10)
+    set_child!(window, box)
     present!(window)
 end
