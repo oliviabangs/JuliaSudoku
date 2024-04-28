32a2b90cdb34b21c4e317fe565374635211ff95e
Mary margaret Cooney <marymargaretcooney@marys-air-4.trinity.local>
2024-04-28 14:40:44 -0500

    working on the adding button. Able to add them and just trying to figure out how to connect them to board to see if we can switch to a new one

--- gui.jl
+++ gui.jl
@@ -1,12 +1,55 @@
 using Mousetrap
+
+function newboard_button(board)::Button
+    newBoard = Button(Label("New Board"))
+    set_size_request!(newBoard,Vector2f(50,100))
+    set_vertical_alignment!(newBoard,ALIGNMENT_CENTER)
+    set_horizontal_alignment!(newBoard,ALIGNMENT_START)
+    board = []
+    connect_signal_clicked!(on_clicked, newBoard, board)
+    return newBoard
+end
+
+
+function on_clicked(self::Button,board)
+    println("board:" $board)
+end
+
 main() do app::Application
     window = Window(app)
 
-    button = Button()
-    connect_signal_clicked!(button) do self::Button
-        println("clicked")
+    board = [0,0];
+    
+    newBoard = newboard_button(board);
+    clearBoard = Button(Label("Clear Board"))
+    exit = Button(Label("Exit"))
+
+    connect_signal_clicked!(newBoard,exit) do newBoard::Button, exit::Button
+        println("newBoard clicked")
+
+        set_signal_clicked_blocked!(newBoard,true)
+        emit_signal_clicked(exit)
+        set_signal_clicked_blocked!(newBoard,false)
+
     end
-    set_child!(window,button)
+
+    connect_signal_clicked!(exit,newBoard) do exit::Button,newBoard::Button
+        println("exit clicked")
+
+        set_signal_clicked_blocked!(exit,true)
+        emit_signal_clicked(newBoard)
+        set_signal_clicked_blocked!(exit,false)
+    end
+
+    connect_signal_clicked!(clearBoard,newBoard) do clearBoard::Button,newBoard::Button
+        println("clearBoard clicked")
+
+        set_signal_clicked_blocked!(clearBoard,true)
+        emit_signal_clicked(newBoard)
+        set_signal_clicked_blocked!(clearBoard,false)
+
+    end
+
+    set_child!(window, hbox(newBoard,exit,clearBoard))
     present!(window)
 end
-
